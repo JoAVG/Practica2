2d1b370bba64c21b0c708914614f07e23cebd09e
JoAVG <alfredopooo02@gmail.com>
2022-09-16 00:27:14 -0500

    Update NumCapicua.java

--- src/problems/NumCapicua.java
+++ src/problems/NumCapicua.java
@@ -1,13 +1,20 @@
 package problems;
 
 public class NumCapicua {
-	public boolean numCapicua(int num) {
-        int falta=num, numInvertido=0;
-        while (falta>0) {
-            int resto = falta % 10;
-            falta = falta / 10;
-            numInvertido = numInvertido * 10 + resto;
+	public boolean numCapicua(int num) { 	
+        int falta=num, numInvertido=0; //C.E. (1)   //C.T. (1)
+        int resto;                     //C.E. (1)   //C.T. (1)
+        while (falta>0) {							//C.T. (n)
+            resto = falta % 10;						//C.T. (1)
+            falta = falta / 10;                     //C.T. (1)
+            numInvertido = numInvertido * 10 + resto; //C.T. (1)
         }
-        return (numInvertido==num);
+        return (numInvertido==num);                    //C.T. (1)
     }
 }
+
+//Complejidad Espacial = 1+1 = 2 "Se declararon 2 variables extras"
+//Complejidad Temporal = 1+1+(n(1+1+1))+1 = 3 + 3n = O(n) una complejidad Lineal.
+
+//Complejidad Espacial = 1+1 = 2 "Se declararon 2 variables extras"
+//Complejidad Temporal = 1+1+(n(1+1+1))+1 = 3 + 3n = O(n) una complejidad Lineal.
